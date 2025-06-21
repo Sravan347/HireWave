5a3c03077d5f3c9ce7ac34a4aa2e4fe971bfa83f
sravan ks <sravansreyas3@gmail.com.com>
2025-06-21 13:35:01 +0530

    Set up and configure tailwind css for the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ const App = () => {
   return (
     <div>
       <h3>client side</h3>
+        <h1 class="text-3xl font-bold underline text-red-700">
+    Hello world!
+  </h1>
      
     </div>
   )
