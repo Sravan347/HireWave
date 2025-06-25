7f3e7c0b3fd99deab7c7628b5d5453ee74cfa96e
sravan ks <sravansreyas3@gmail.com.com>
2025-06-25 14:54:57 +0530

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,84 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from './components/ui/input'
-const App = () => {
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
+import Jobs from './components/Jobs'
+import Browse from './components/Browse'
+import Profile from './components/Profile'
+import JobDescription from './components/JobDescription'
+import Companies from './components/admin/Companies'
+import CompanyCreate from './components/admin/CompanyCreate'
+import CompanySetup from './components/admin/CompanySetup'
+import AdminJobs from "./components/admin/AdminJobs";
+import PostJob from './components/admin/PostJob'
+import Applicants from './components/admin/Applicants'
+import ProtectedRoute from './components/admin/ProtectedRoute'
+
+
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+  {
+    path: "/jobs",
+    element: <Jobs />
+  },
+  {
+    path: "/description/:id",
+    element: <JobDescription />
+  },
+  {
+    path: "/browse",
+    element: <Browse />
+  },
+  {
+    path: "/profile",
+    element: <Profile />
+  },
+ 
+  {
+    path:"/admin/companies",
+    element: <ProtectedRoute><Companies/></ProtectedRoute>
+  },
+  {
+    path:"/admin/companies/create",
+    element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/companies/:id",
+    element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs",
+    element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs/create",
+    element:<ProtectedRoute><PostJob/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs/:id/applicants",
+    element:<ProtectedRoute><Applicants/></ProtectedRoute> 
+  },
+
+])
+function App() {
+
   return (
     <div>
-      <h3>client side</h3>
-        <h1 class="text-3xl font-bold underline text-blue-700">
-    Hello world!
-  </h1>
-     <Button>click</Button>
-
-     <Input/>
-     <Button>click</Button>
-     <Button>click</Button>
+      <RouterProvider router={appRouter} />
     </div>
   )
 }
