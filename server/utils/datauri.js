7cd379ea09aef8e99888656cd59b58740285e8f0
sravan ks <sravansreyas3@gmail.com.com>
2025-06-25 17:01:28 +0530

    update signup

--- server/utils/datauri.js
+++ server/utils/datauri.js
@@ -1,14 +1,10 @@
-
-
-// import DataUriParser from "datauri/parser.js"
-const DataUriParser = require("datauri/parser.js").default;
-
+const DataUriParser = require("datauri/parser"); // ✅ fixed import
 const path = require("path");
 
 const getDataUri = (file) => {
     const parser = new DataUriParser();
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName, file.buffer);
-}
+};
 
 module.exports = getDataUri;
