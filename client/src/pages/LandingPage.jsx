8d1435d9a407e982f38511a6da1ea7d434119d21
sravan ks <sravansreyas3@gmail.com.com>
2025-07-01 16:20:21 +0530

    shadcn configure

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,23 +1,7 @@
-// import React from 'react';
-// import Navbar from '../components/Navbar';
-// import logo from '../assets/logo.png';
 
-// const LandingPage = () => {
-//   return (
-//     <div className="bg-white min-h-screen">
-//       <Navbar />
-//       <div className="p-10 text-center">
-//         <img src={logo} alt="HireWave Logo" className="mx-auto mb-6 w-32 h-32 object-contain" />
-//         <h1 className="text-4xl font-bold text-[#2563EB]">Welcome to HireWave</h1>
-//         <p className="text-gray-600 mt-4">Your AI-powered recruitment assistant</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LandingPage;
 
 import React, { useEffect, useState } from 'react';
+import { Button } from "@/components/ui/button"
 
 import API from '../services/api';
 import Navbar from '../components/Navbar';
@@ -79,6 +63,8 @@ const LandingPage = () => {
           ) : (
             <p className="text-center mt-10 text-gray-500">No jobs found.</p>
           )}
+
+        
         </div>
       </main>
       <Footer /> {/* Add footer at the bottom */}
